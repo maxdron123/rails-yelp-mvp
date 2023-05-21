0e9bf2088dff5c9fde9bb7e0b90dc41189070597
Maksym Dronov <simbanyan98@gmail.com>
2023-05-21 18:52:12 +0300

    added reviews model + create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: %i[create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database'
 Restaurant.destroy_all if Rails.env.development?
 category = %w[chinese italian japanese french belgian]
 6.times do |i|
